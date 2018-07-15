35708bffc2f30db938ceacfaae587b22f5f1d0ec
tesdeve <tesdeve@gmail.com>
2018-07-15 11:53:13 +0100

    1st Commit with .gitignore

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'pages/contact'
+  get 'pages/privacy_policy'
+  get 'pages/legal'
+  get 'pages/cookie_policy'
+  get 'pages/unauthorised'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
